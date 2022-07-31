21d2aa1c3f2ec4531ed5e4186d99e8d4b4c3c497
Eduardo Dubeux <89784543+Edudubeux@users.noreply.github.com>
2022-07-31 05:55:11 -0300

    Add files via upload

---
+++ Puzzle/src/Tela.java
@@ -0,0 +1,9 @@
+public class Tela {
+    private tabuleiro;
+    private jogador;
+    private telaTabuleiro;
+    private botaoMovimentoCima;
+    private botaoMovimentoBaixo;
+    private botaoMovimentoDireita;
+    private botaoMovimentoEsquerda;
+}
