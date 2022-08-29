86f872df03c494baeb8f6486282d06697fe35893
pItico53 <eduardodubeux1a@gmail.com>
2022-08-29 19:47:54 -0300

    Terminando o checkpoint 2

--- Puzzle/src/Tabuleiro.java
+++ Puzzle/src/Tabuleiro.java
@@ -1,7 +1,25 @@
-public class Tabuleiro {
+import Settings.Menu;
+
+public class Tabuleiro extends Settings.Menu {
     private int celulaVazia;
-    private int celulas;
-    private int[][] tabuleiro = {{1,3}, {1,3}};
+    private int quantCelulas;
+    private int tabuleiro[];
 
+    public Tabuleiro(int dificuldade) {
+        super(dificuldade);
 
+        if (dificuldade == 1) {
+            this.quantCelulas = 4;
+        } else if (dificuldade == 2) {
+            this.quantCelulas = 8;
+        } else {
+            this.quantCelulas = 12;
+        }
+    }
+
+    public void createTabuleiro (){
+        for (int i = 0; i < this.quantCelulas; i++) {
+            tabuleiro[i] = i + 1;
+        }
+    }
 }
--- Puzzle/src/Users/Jogadores.java
+++ Puzzle/src/Users/Jogadores.java
@@ -1,3 +1,5 @@
+package Users;
+
 public class Jogadores {
     private int[][] jogadores = {{}};
 }
